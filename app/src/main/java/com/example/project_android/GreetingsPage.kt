95e797abb2e5e5b0fbefd75fb76b3dd15ed1d6b6
Sunrise443 <sunkensun0@gmail.com>
2025-04-06 21:47:59 +0300

    added theme and fixed greetings page for better

--- app/src/main/java/com/example/project_android/GreetingsPage.kt
+++ app/src/main/java/com/example/project_android/GreetingsPage.kt
@@ -1,68 +1,88 @@
 package com.example.project_android
 
+// can we remove the imports and replace it with .*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Preview(showBackground = true)
 @Composable
 fun GreetingsPage(){
-    Box(
-        modifier = Modifier
-            .background(Color.White)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            modifier = Modifier
-                .align(Alignment.TopCenter),
-            painter = painterResource(id = R.drawable.greetings_dog),
-            contentDescription = null
-        )
-        Column (
-            modifier = Modifier
-                .padding(30.dp, 30.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Добро пожаловать в",
-                fontSize = 30.sp,
-                modifier = Modifier
-                    .padding(10.dp)
+    Scaffold(
+        topBar = {
+            Image(
+                painter = painterResource(id = R.drawable.greetings_dog),
+                contentDescription = null
             )
-            Text(
-                text = "НАЗВАНИЕ ПРИЛОЖЕНИЯ",
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .padding(10.dp)
-            )
-            Text(
-                text = "Выберите изображение и узнайте, что на нем изображено.",
-                fontSize = 30.sp,
-                modifier = Modifier
-                    .padding(10.dp)
-            )
+        },
+        bottomBar = {
             Button(
                 onClick = {
 
                 },
-
+                modifier = Modifier
+                    .defaultMinSize(minHeight = 80.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(0)
                 ) {
-                Text(text = "Продолжить")
+                Text(text = "Продолжить",
+                    fontSize = 25.sp,
+                    fontFamily = FontFamily.Monospace
+                    )
+            }
+        },
+        content = { innerPadding ->
+            Column (
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                ){
+                Text(
+                    text = "Добро пожаловать в",
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+                Text(
+                    text = "DogFounder",
+                    fontSize = 40.sp,
+                    modifier = Modifier
+                        .padding(10.dp),
+                    fontWeight = FontWeight.SemiBold,
+                )
+                Spacer(Modifier
+                    .size(30.dp))
+                Text(
+                    text = "Выберите фото и узнайте, что на нем изображено.",
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .padding(10.dp),
+                    textAlign = TextAlign.Center
+                )
             }
         }
-    }
+    )
 }
+
